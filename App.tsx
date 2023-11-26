ea4265da7069488c9897e59b4b2826b4058507d3
DevVitorPlay <vitor.abreu@playmovel.com.br>
2023-11-25 21:04:24 -0300

    importando fonts do google expo fonts

--- App.tsx
+++ App.tsx
@@ -1,12 +1,20 @@
 import { Groups } from '@screens/Groups';
 import theme from './src/theme';
 import { ThemeProvider } from 'styled-components';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { ActivityIndicator } from 'react-native';
+import { Loading } from '@components/Loading';
 
 export default function App() {
+  const [fontLoader] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  })
+
   return (
    
     <ThemeProvider theme={theme}>
-        <Groups/>
+        {!fontLoader ? <Groups/> : <Loading/>}
     </ThemeProvider>
    
   );
