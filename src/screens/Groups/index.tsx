0cfc52af573f86cc585eda9e633989418737da3c
DevVitorPlay <vitor.abreu@playmovel.com.br>
2023-11-28 14:13:29 -0300

    adicionando componente ListEmpty

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,9 +4,10 @@ import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
 import { useState } from 'react';
 import { FlatList } from 'react-native';
+import { ListEmpty } from '@components/ListEmpty';
 
 export function Groups() {
-  const [groups, setGroups] = useState(['Futebol', 'Videogame'])
+  const [groups, setGroups] = useState([])
   return (
     <Container>
       <Header />
@@ -22,6 +23,10 @@ export function Groups() {
           <GroupCard
             title={item}
           />
+        )}
+        contentContainerStyle={groups.length === 0 &&{flex: 1}}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal começar um novo grupo?"/>
         )}
       />
       
