42665c1a53aa17d6340144999dcbeaa044af7da1
DevVitorPlay <vitor.abreu@playmovel.com.br>
2023-11-28 11:48:41 -0300

    estrutura da flatlist

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,17 +1,30 @@
 import { Header } from '@components/Header';
-import { Container} from './styles';
+import { Container } from './styles';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
+import { useState } from 'react';
+import { FlatList } from 'react-native';
 
 export function Groups() {
+  const [groups, setGroups] = useState(['Futebol', 'Videogame'])
   return (
     <Container>
-      <Header/>
+      <Header />
       <Highlight
         title='Turmas'
         subtitle='Jogue com sua turma'
       />
-      <GroupCard title='Futebol da Play' />
+
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard
+            title={item}
+          />
+        )}
+      />
+      
     </Container>
   );
 }
