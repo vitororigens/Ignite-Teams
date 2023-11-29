db11e82584cd086a45c8b4c6e198722ab421c589
DevVitorPlay <vitor.abreu@playmovel.com.br>
2023-11-29 10:27:04 -0300

    refatorando o codigo com csshelper

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -14,7 +14,9 @@ export function NewGroup() {
                     title="Nova Turma"
                     subtitle="crie sua turma para adicionar pessoas"
                 />
-                <Input />
+                <Input
+                    placeholder="Nome da turma"
+                />
                 <Button
                     title="Criar"
                     style={{
