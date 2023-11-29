d0882222a374692f1b2e9028c82c58fbc1ac4de9
DevVitorPlay <vitor.abreu@playmovel.com.br>
2023-11-29 09:34:25 -0300

    criando a interface NewGroup

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { Groups } from '@screens/Groups';
+import { NewGroup } from '@screens/NewGroup';
 import theme from './src/theme';
 import { StatusBar } from 'react-native'
 import { ThemeProvider } from 'styled-components';
@@ -20,7 +20,7 @@ export default function App() {
       translucent
       
       />
-        {fontLoader ? <Groups/> : <Loading/>}
+        {fontLoader ? <NewGroup/> : <Loading/>}
     </ThemeProvider>
    
   );
